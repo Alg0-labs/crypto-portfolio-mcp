42d6f8ea008826aac958272ee2d86f44ba4ddf31
Vibhu Dixit <dixitvibhu20@gmail.com>
2026-01-08 13:59:34 +0530

    chore: Added the MoralisClient

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,10 @@
-
 from .coinmarketcap import CoinMarketCapClient, CoinMarketCapAPIError
+from .blockchain import MoralisClient, BlockchainClient, BlockchainAPIError
 
 __all__ = [
     "CoinMarketCapClient",
     "CoinMarketCapAPIError",
+    "MoralisClient",
+    "BlockchainClient",
+    "BlockchainAPIError",
 ]
